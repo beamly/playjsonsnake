3554dd09c79dd7fc035614a326de379012b64421
Dale Wijnand <dale.wijnand@gmail.com>
2016-02-29 14:26:56 +0000

    Implement compile-time casing
    Includes:
    
    * make camelCase/snakeCase utils accessible
    * use macro-compat to gracefully support multiple Scala versions and allow for
      quasiquotes
    * deprecate old methods, for migration purposes and to maintain bincompat

--- src/main/scala/playjsonsnake/SnakeCaseJson.scala
+++ src/main/scala/playjsonsnake/SnakeCaseJson.scala
@@ -1,22 +1,31 @@
 package playjsonsnake
 
+import scala.language.experimental.macros
+
 import play.api.libs.json._
 
 object SnakeCaseJson {
+  @deprecated("Use version that changes casing at compile time", "1.1.0")
   def  reads[T](fjs:   Reads[T]):   Reads[T] = Reads.of[JsObject] map camelCaseFields andThen fjs
+  @deprecated("Use version that changes casing at compile time", "1.1.0")
   def writes[T](tjs: OWrites[T]): OWrites[T] = OWrites[T](x => snakeCaseFields(tjs writes x))
+  @deprecated("Use version that changes casing at compile time", "1.1.0")
   def format[T](f:   OFormat[T]): OFormat[T] = OFormat[T](reads(f), writes(f))
+
+  def  reads[T]:   Reads[T] = macro SnakeCaseJsonMacros.readsImpl[T]
+  def writes[T]: OWrites[T] = macro SnakeCaseJsonMacros.writesImpl[T]
+  def format[T]: OFormat[T] = macro SnakeCaseJsonMacros.formatImpl[T]
 
   private def camelCaseFields(json: JsObject) = JsObject(json.fields map (kv => camelCase(kv._1) -> kv._2))
   private def snakeCaseFields(json: JsObject) = JsObject(json.fields map (kv => snakeCase(kv._1) -> kv._2))
 
-  private def camelCase(s: String) =
+  private[playjsonsnake] def camelCase(s: String) =
     (s.split("_").toList match {
       case head :: tail => head :: tail.map(_.capitalize)
       case x            => x
     }).mkString
 
-  private def snakeCase(s: String) =
+  private[playjsonsnake] def snakeCase(s: String) =
     s.foldLeft(new StringBuilder) {
       case (s, c) if Character.isUpperCase(c) && s.nonEmpty => s append "_" append (Character toLowerCase c)
       case (s, c)                                           => s append c
